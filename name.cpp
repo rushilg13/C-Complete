86ae368d2f3bdf4eb59da21f473fb14ec3a65e87
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-30 19:52:06 +0530

    Added Comments

--- name.cpp
+++ name.cpp
@@ -6,6 +6,9 @@ int main()
 	string name;
 	cout<<"Enter Your Name ";
 	getline(cin, name);
+	// We could have used cin >> name
+	// But cin only takes a word as input. so if input was Raj Kumar, only Raj would have been stored and output would be Hello, Raj
+	// getline(cin, variable) reads input till \n is not encountred aka complete line is read.
 	cout<<"Hello, "<<name;
 	return 0;
 }

9e3c7b77d0abe2efcb58692acf70fe6a579697c6
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-11-30 20:10:00 +0530

    Added Files

---
+++ math.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	int a;
+	cout<<"Enter a number\n";
+	cin>>a;
+	cout<<"Square of a is " << pow(a,2);
+
+	int b;
+	cout<<"Enter a number\n";
+	cin>>b;
+	cout<<"Square Root of b is " << sqrt(b);
+
+}

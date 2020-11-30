b817fa67d126a0fae424e28244f5e98aeee08767
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-01 01:19:57 +0530

    Added files

---
+++ switchcase.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a;
+	cin>>a;
+	switch(a)
+	{
+
+		case 1: cout<<"Odd";
+		break;
+		case 2: cout<<"Even";
+		break;
+		case 3: cout<<"Odd";
+		break;
+		case 4: cout<<"Even";
+		break;		
+		case 5: cout<<"Odd";
+		break;
+		case 6: cout<<"Even";
+		break;
+		case 7: cout<<"Odd";
+		break;
+		case 8: cout<<"Even";
+		break;
+		case 9: cout<<"Odd";
+		break;
+		case 10: cout<<"Even";
+		break;	
+		default: cout<<"Invalid";	
+	}
+	return 0;
+}

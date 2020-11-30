0aff9ab24b2d72a48831e960903b6b39e1fe41f8
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-01 01:07:02 +0530

    Added Files

--- conditional.cpp
+++ conditional.cpp
@@ -12,5 +12,16 @@ int main(){
 	{
 		cout<<b<<" is greater";
 	}
+
+	if(0)
+	{
+		cout<<"This code is not printed as 0 means false in if block";
+	}
+
+	if(10000)
+	{
+		cout<<"This code is printed as all numbers except 0 means True in if block";
+	
+	}
 	return 0;
 }

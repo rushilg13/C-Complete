2b9aeb30b3b743c74de76ce755760fb9e5e25ee9
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-01 13:28:18 +0530

    Added Prog

--- func_pointer.cpp
+++ func_pointer.cpp
@@ -12,3 +12,36 @@ int main(){
 	(*fp)();     // Function call
 	return 0;
 }
+
+//////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+int max(int x, int y)
+{
+	if (x>y)
+	cout<<x<<endl;
+	else
+	cout<<y<<endl;
+}
+
+int min(int x, int y)
+{
+	if (x<y)
+	cout<<x<<endl;
+	else
+	cout<<y<<endl;
+}
+
+int main()
+{
+	int a=10, b=12;
+	int (*fp)(int, int);
+	fp = max;
+	(*fp)(a,b);
+	fp = min;
+	(*fp)(a,b);
+}
+
+////////////////////////////////////////////////////////////////

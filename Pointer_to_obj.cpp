a04cce77c8c4f171036b1d4759c77ba07ff2ffbd
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-01 16:59:28 +0530

    Added Files

---
+++ Pointer_to_obj.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class Rectangle
+{
+    public:
+    float l;
+    float b;
+
+	float area()
+	{
+		return l*b;
+	}
+
+	float perimeter()
+	{
+		return 2*(l+b);
+	}
+};
+
+
+int main(int argc, char const *argv[])
+{
+	Rectangle r1;
+	r1.l = 10;
+	r1.b = 20;
+	float a=r1.area();
+	cout<<a<<endl;
+	a=r1.perimeter();
+	cout<<a<<endl;
+
+	Rectangle *p;
+	p->l = 20;
+	p->b = 9;
+	cout<<p->area()<<endl;
+	cout<<p->perimeter();
+	return 0;
+}

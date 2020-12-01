6bf5e2db1ead860d7f56f5db259850918c3f278b
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2020-12-01 12:42:35 +0530

    Added files

---
+++ reference.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int a_big_named_variable =10;
+	int &y = a_big_named_variable;  // declaration has to be done when its initialized
+	p = &a;
+	cout<<"a_big_named_variable: "<<a_big_named_variable<<endl;
+	cout<<"y "<<y<<endl;
+
+	return 0;
+}
